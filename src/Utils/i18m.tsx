41b7ba1b486fba49518401f4b6e6910ee2a982aa
Victor Hernandez <victor.hernandez7611@alumnos.udg.mx>
2023-02-14 23:22:57 -0600

    added basic structure to use multiculture

---
+++ src/Utils/i18m.tsx
@@ -0,0 +1,23 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import en from './en.json';
+import es from './es.json';
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    resources: {
+      en: en,
+      es: es,
+    },
+    react :{
+      useSuspense:false,
+    },
+    compatibilityJSON: 'v3',
+    lng: "en",
+    interpolation: {
+      escapeValue: false
+    }
+  });
+
+export default i18n;
