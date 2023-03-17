9ebdc98445b0ec3ac55b4076107fc3f6f4749357
Victor Hernandez <73491889+VictorVMHC@users.noreply.github.com>
2023-03-16 23:50:45 -0600

    added context and examples
    the examples needs to be removed

--- src/Views/MapsView.tsx
+++ src/Views/MapsView.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { useTranslation } from 'react-i18next';
+import { Button, Text, View } from 'react-native'
 
 export const MapsView = () => {
+    const {t, i18n} = useTranslation();
+    const changeLenguage =(value: string) =>{
+        i18n.changeLanguage(value);
+        console.log(value);
+    }
     return (
         <View>
             <Text> Maps view </Text>
+            <View>
+                <Text> {t('Hello World')} </Text>
+                <Button
+                title={t('Hello World')}
+                onPress={() => changeLenguage('es')}
+                />
+            </View> 
         </View>
     )
 }
