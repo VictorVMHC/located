a7911dc3eb4a48f0109d3e67c417f923d3237217
LuisDani-Zamora <89962446+LuisDani-Zamora@users.noreply.github.com>
2023-03-22 17:48:45 -0600

    DrawerMenuTryStyle
    agregamos una foto bien perra

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
+import 'react-native-gesture-handler';
 import './src/Utils/i18n';
 
 
 import React from 'react';
 import { I18nextProvider , useTranslation } from 'react-i18next';
 import { Button , Text , View } from 'react-native';
-import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
-import 'react-native-gesture-handler';
 import { MainStackNavigator } from './src/Navigation/MainStackNavigator';
 import { TabBarNavigator } from './src/Navigation/TabBarNavigatior';
 import { DrawerMenu} from './src/Navigation/DrawerMenu';
