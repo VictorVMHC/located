7e2db2bc239e5be236e4c5a18b68ff10e2901ca1
Victor Hernandez <73491889+VictorVMHC@users.noreply.github.com>
2023-05-08 22:37:16 -0600

    added localization functionality

--- src/Utils/i18n.tsx
+++ src/Utils/i18n.tsx
@@ -1,21 +1,22 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import { ReactNativeLanguageDetector } from 'react-native-localization-settings';
 
 import en from './en.json';
 import es from './es.json';
 
 i18n
+  .use(ReactNativeLanguageDetector)
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources: {
-      en: en,
-      es: es,
+      "en-US": en,
+      "es-MX": es,
     },
     react :{
       useSuspense:false,
     },
     compatibilityJSON: 'v3',
-    lng: "en",
     interpolation: {
       escapeValue: false
     }
