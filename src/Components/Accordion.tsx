24c697780c10b632f1b1109aa635d1ebb411a6e2
LuisDani-Zamora <89962446+LuisDani-Zamora@users.noreply.github.com>
2023-08-07 01:47:51 -0600

    HelpView fisrt look

--- src/Components/Accordion.tsx
+++ src/Components/Accordion.tsx
@@ -17,13 +17,16 @@ export const Accordion = ({question, answer,}:Props) => {
     return (
         <View>
             <TouchableOpacity onPress={toggle}>
-                <Text style={StylesFAQ.titleQuestion}>{question} 
-                <Icon name='plus' size={35} color={Colors.black} />
-                </Text>
-            {expanded && <View>{
-             <Text style={StylesFAQ.answer}>{answer}</Text>
-             }</View>}
-            </TouchableOpacity>   
+            {!expanded
+                ?<Text style={StylesFAQ.titleQuestion}>{question} 
+                <Icon name='chevron-down' size={20} color={Colors.black} /></Text>
+                :<Text style={StylesFAQ.titleQuestion}>{question} 
+                <Icon name='chevron-up' size={20} color={Colors.black} /></Text>
+            }
+                {expanded && <View>{
+                    <Text style={StylesFAQ.answer}>{answer}</Text>
+        }</View>}
+            </TouchableOpacity>
             <View style={StylesFAQ.separate}></View>
         </View>
     )
--- src/Views/HelpView.tsx
+++ src/Views/HelpView.tsx
@@ -1,9 +1,10 @@
 import React, {useState, useEffect} from 'react'
-import { Text, View, Image, TouchableOpacity, StyleSheet, LayoutAnimation} from 'react-native'
+import { Text, View, Image, TouchableOpacity, StyleSheet,TextInput} from 'react-native'
 import { useTranslation } from 'react-i18next';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { FlatList, ScrollView } from 'react-native-gesture-handler';
 import {Accordion} from '../Components/Accordion';
+import { Colors, Styles } from '../Themes/Styles'
 
 interface Data {
     id: number;
@@ -12,22 +13,11 @@ interface Data {
 }
 
 const listArray: Data[] = [
-    { id: 1, question: '¿POR QUÉ NO PUEDO ACCEDER A TODAS LAS FUNCIONES DE LA APP? ', answer: 'Si no puedes ingresar a todas las funciones de la app necesitas estar registrado o haber iniciado sesion '},
-    { id: 2, question: '¿POR QUÉ NO PUEDO ACCEDER A TODAS LAS FUNCIONES DE LA APP?  +', answer: 'Si no puedes ingresar a todas las funciones de la app necesitas estar registrado o haber iniciado sesion '},
-    { id: 3, question: '¿POR QUÉ NO PUEDO ACCEDER A TODAS LAS FUNCIONES DE LA APP?  +', answer: 'Si no puedes ingresar a todas las funciones de la app necesitas estar registrado o haber iniciado sesion '},
-    { id: 4, question: '¿POR QUÉ NO PUEDO ACCEDER A TODAS LAS FUNCIONES DE LA APP?  +', answer: 'Si no puedes ingresar a todas las funciones de la app necesitas estar registrado o haber iniciado sesion '},
-    { id: 5, question: '¿POR QUÉ NO PUEDO ACCEDER A TODAS LAS FUNCIONES DE LA APP?  +', answer: 'Si no puedes ingresar a todas las funciones de la app necesitas estar registrado o haber iniciado sesion '},
-    { id: 6, question: '¿POR QUÉ NO PUEDO ACCEDER A TODAS LAS FUNCIONES DE LA APP?  +', answer: 'Si no puedes ingresar a todas las funciones de la app necesitas estar registrado o haber iniciado sesion '},
-    { id: 7, question: '¿POR QUÉ NO PUEDO ACCEDER A TODAS LAS FUNCIONES DE LA APP?  +', answer: 'Si no puedes ingresar a todas las funciones de la app necesitas estar registrado o haber iniciado sesion '},
-    { id: 8, question: '¿POR QUÉ NO PUEDO ACCEDER A TODAS LAS FUNCIONES DE LA APP?  +', answer: 'Si no puedes ingresar a todas las funciones de la app necesitas estar registrado o haber iniciado sesion '},
-    { id: 9, question: '¿POR QUÉ NO PUEDO ACCEDER A TODAS LAS FUNCIONES DE LA APP?  +', answer: 'Si no puedes ingresar a todas las funciones de la app necesitas estar registrado o haber iniciado sesion '},
-    { id: 10, question: '¿POR QUÉ NO PUEDO ACCEDER A TODAS LAS FUNCIONES DE LA APP?  +', answer: 'Si no puedes ingresar a todas las funciones de la app necesitas estar registrado o haber iniciado sesion '},
-    { id: 11, question: '¿POR QUÉ NO PUEDO ACCEDER A TODAS LAS FUNCIONES DE LA APP?  +', answer: 'Si no puedes ingresar a todas las funciones de la app necesitas estar registrado o haber iniciado sesion '},
-    { id: 12, question: '¿POR QUÉ NO PUEDO ACCEDER A TODAS LAS FUNCIONES DE LA APP?  +', answer: 'Si no puedes ingresar a todas las funciones de la app necesitas estar registrado o haber iniciado sesion '},
-    { id: 13, question: '¿POR QUÉ NO PUEDO ACCEDER A TODAS LAS FUNCIONES DE LA APP?  +', answer: 'Si no puedes ingresar a todas las funciones de la app necesitas estar registrado o haber iniciado sesion '},
-    { id: 14, question: '¿POR QUÉ NO PUEDO ACCEDER A TODAS LAS FUNCIONES DE LA APP?  +', answer: 'Si no puedes ingresar a todas las funciones de la app necesitas estar registrado o haber iniciado sesion '},
-    
-    
+    { id: 1, question: '¿POR QUÉ NO PUEDO ACCEDER A TODAS LAS FUNCIONES DE LA APP?', answer: 'Si no puedes ingresar a todas las funciones de la app necesitas estar registrado o haber iniciado sesion '},
+    { id: 2, question: '¿POR QUÉ NO PUEDO ACCEDER A TODAS LAS FUNCIONES DE LA APP?', answer: 'Si no puedes ingresar a todas las funciones de la app necesitas estar registrado o haber iniciado sesion '},
+    { id: 3, question: '¿POR QUÉ NO PUEDO ACCEDER A TODAS LAS FUNCIONES DE LA APP?', answer: 'Si no puedes ingresar a todas las funciones de la app necesitas estar registrado o haber iniciado sesion '},
+    { id: 4, question: '¿POR QUÉ NO PUEDO ACCEDER A TODAS LAS FUNCIONES DE LA APP?', answer: 'Si no puedes ingresar a todas las funciones de la app necesitas estar registrado o haber iniciado sesion '},
+    { id: 5, question: '¿POR QUÉ NO PUEDO ACCEDER A TODAS LAS FUNCIONES DE LA APP?', answer: 'Si no puedes ingresar a todas las funciones de la app necesitas estar registrado o haber iniciado sesion '},
 ];
 
 const rendererData = ({ item }: { item: Data }) => {
@@ -44,45 +34,60 @@ export const HelpView = () => {
 
 
     return (
-        <View style={StylesFAQ.container}>
+        <SafeAreaView>
+            <ScrollView>
+        <View  style={StylesFAQ.container}>
             <View>
-                <Image source={require('../Assets/Images/helpImage.png')} style={{width: 200, height: 190, borderRadius: 5, left:'21%'}} />
+                <Image source={require('../Assets/Images/helpImage.png')} style={{width: 200, height: 190, borderRadius: 5,}} />
             </View>
+            <Text style={StylesFAQ.titleText}>{t('FAQ')}</Text>
             <View>
                 <FlatList data={listArray} renderItem={rendererData}/>
             </View>
         </View>
+                <View style={StylesFAQ.secondContainer}>
+                    <Text style={StylesFAQ.secondText}>{t('InfoForAsk')}</Text>
+                    <TextInput style={{...Styles.input, width:310, fontSize:20, top:1}}
+                        placeholderTextColor={Colors.blueText}
+                        placeholder={`${t('WriteQuestion')}`}   
+                    />
+                    <TouchableOpacity style={{...Styles.boton,top:4, borderRadius:10,width:310}}>
+                        <Text style={{...Styles.txtBtn,top:1}}>{t('Send')}</Text>
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
 
 const StylesFAQ = StyleSheet.create({
     container:{
-        flex:2,
+        flex:1,
         alignItems:'center',
         padding:10,
     },
     titleText:{
-        fontSize:30,
-        fontWeight:'bold',
-        padding:10,
-        marginVertical:10,
-        top:-10,
-    },
-    titleQuestion:{
-        fontSize:14,
-        fontWeight:'bold',
-        color:'black',
-        width:320,
-    },
-    answer:{
         fontSize:22,
-        fontFamily:'Outfit.Light',
-        textAlign:'left',
-        width:320,
+        color:'black',
+        fontWeight:'bold',
+        padding:4,
+        top:-10,
     },
     separate:{
         backgroundColor:'rgba(255,198,0,0.4)',
         height:3,
         width:"98%",
-    }
+    },
+    secondContainer:{
+        flex:1,
+        alignItems:'center',
+        padding:10,
+        top:-8,
+    },
+    secondText:{
+        fontSize:20,
+        fontFamily:'Outfit.Regular',
+        color:'black',
+        padding:4,
+    },
 })
