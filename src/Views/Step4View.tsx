498fee7887cd1b3de2fa8057c193a8232a6570a0
Victor Hernandez <73491889+VictorVMHC@users.noreply.github.com>
2023-08-13 23:59:51 -0600

    Added time picker

--- src/Views/Step4View.tsx
+++ src/Views/Step4View.tsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
-import { KeyboardAvoidingView, Text, View } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import { Button, KeyboardAvoidingView, StyleSheet, Text, View } from 'react-native';
+import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
 import { PickerDayButton } from '../Components/PickerDayButton';
 import { Schedule } from '../Interfaces/DbInterfaces';
+import DatePicker from 'react-native-date-picker';
 
 export const Step4View = () => {
 
     const [schedule, setSchedule] = useState<Schedule[]>([]);
+    const [date, setDate] = useState(new Date())
+    const [open, setOpen] = useState(false)
 
     const updateScheduleItem = (scheduleNumber: number, updateData: Partial<Schedule>) => {
         const updatedSchedule = [...schedule];
@@ -18,18 +21,53 @@ export const Step4View = () => {
     };
 
     return (
-        <KeyboardAvoidingView>
-            <ScrollView>
-                <View>
-                    <PickerDayButton
-                        title={'Del dia'}
-                        day='day2'
-                        action={updateScheduleItem}
-                        index={1}
-                    />
+        <KeyboardAvoidingView behavior="padding" style={styles.container} >
+            <ScrollView style={{ flexGrow: 1}}>
+                <View style={{backgroundColor: 'blue'}}>
+                    <View style={{backgroundColor: 'red'}}>
+                        <PickerDayButton
+                            title={'Del dia'}
+                            day='day1'
+                            action={updateScheduleItem}
+                            index={1}
+                        />
+                        <PickerDayButton
+                            title={'Del dia'}
+                            day='day1'
+                            action={updateScheduleItem}
+                            index={1}
+                        />
+                    </View>
+                    <View style={{backgroundColor: 'red'}}>
+                        <TouchableOpacity title="Open" onPress={() => setOpen(true)} >
+                            <Text>Hora</Text>    
+                        </TouchableOpacity>
+                        <DatePicker
+                            modal
+                            mode='time'
+                            open={open}
+                            date={date}
+                            onConfirm={(date) => {
+                                setOpen(false)
+                                setDate(date)
+                            }}
+                            onCancel={() => {
+                                setOpen(false)
+                            }}
+                            style={{backgroundColor:'red'}}
+                            is24hourSource= 'locale'
+                        />
+                        </View>
                 </View>
                 <Text>{JSON.stringify(schedule)}</Text>
             </ScrollView>
         </KeyboardAvoidingView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: '5%',
+    },
+});
