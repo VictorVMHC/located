80a735e0163768bd934654e7ce191d1eb9ae5b59
Victor Hernandez <73491889+VictorVMHC@users.noreply.github.com>
2023-08-13 23:32:50 -0600

    Created piker day

---
+++ src/Components/PickerDayButton.tsx
@@ -0,0 +1,49 @@
+// PickerDayButton
+import { Picker } from '@react-native-picker/picker';
+import React, { useState } from 'react';
+import { Colors, FontStyles } from '../Themes/Styles';
+import { Schedule } from '../Interfaces/DbInterfaces';
+
+interface Props {
+    title: string,
+    action: (index: number,value: Partial<Schedule>) => void,
+    day: 'day1' | 'day2',
+    index: number
+}
+
+export const PickerDayButton = ({ title, action, day, index }: Props) => {
+    const [selectedValue, setSelectedValue] = useState("");
+
+    const daysOfWeek = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
+
+    const handleValueChange = (selectedDay: string) => {
+        if (day === 'day1') {
+            action(index, { day1: selectedDay });
+        }
+        else{
+            action(index, { day2: selectedDay });
+        }
+        setSelectedValue(selectedDay);
+    };
+
+    return (
+        <Picker
+            selectedValue={selectedValue}
+            onValueChange={handleValueChange}
+            style={{ ...FontStyles.SubTitles }}
+            mode='dialog'
+            dropdownIconRippleColor={Colors.blueAqua}
+            dropdownIconColor={Colors.black}
+        >
+            <Picker.Item label="Seleccione un día" value="" style={FontStyles.Information} />
+            {daysOfWeek.map((element) => (
+                <Picker.Item
+                    key={element}
+                    label={element}
+                    value={element}
+                    style={FontStyles.Information}
+                />
+            ))}
+        </Picker>
+    )
+}
--- src/Views/Step4View.tsx
+++ src/Views/Step4View.tsx
@@ -1,8 +1,35 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React, { useState } from 'react';
+import { KeyboardAvoidingView, Text, View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import { PickerDayButton } from '../Components/PickerDayButton';
+import { Schedule } from '../Interfaces/DbInterfaces';
 
 export const Step4View = () => {
+
+    const [schedule, setSchedule] = useState<Schedule[]>([]);
+
+    const updateScheduleItem = (scheduleNumber: number, updateData: Partial<Schedule>) => {
+        const updatedSchedule = [...schedule];
+        updatedSchedule[scheduleNumber] = {
+            ...updatedSchedule[scheduleNumber],
+            ...updateData,
+        };
+        setSchedule(updatedSchedule);
+    };
+
     return (
-        <Text> Step number 4</Text>
+        <KeyboardAvoidingView>
+            <ScrollView>
+                <View>
+                    <PickerDayButton
+                        title={'Del dia'}
+                        day='day2'
+                        action={updateScheduleItem}
+                        index={1}
+                    />
+                </View>
+                <Text>{JSON.stringify(schedule)}</Text>
+            </ScrollView>
+        </KeyboardAvoidingView>
     )
 }
