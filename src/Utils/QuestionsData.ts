971a4dc87d21f00fd47d3027619623158cb78b6e
Luis Daniel Zamora Delgadillo <89962446+LuisDani-Zamora@users.noreply.github.com>
2023-08-17 21:24:03 -0600

    HelpView With Real Questions
    Were added Some really Q&A

--- src/Utils/QuestionsData.ts
+++ src/Utils/QuestionsData.ts
@@ -8,30 +8,46 @@ export const QuestionsEs: AskAndAnswer[] = [
     },
     {   
         id: 2,
-        question: '¿POR QUÉ NO PUEDO ACCEDER A TODAS LAS FUNCIONES DE LA APP?', 
-        answer: 'Si no puedes ingresar a todas las funciones de la app necesitas estar registrado o haber iniciado sesion '
+        question: '¿POR QUÉ NO APARECE UN NEGOCIO O LOCAL EN ESPECIFÍCO QUE CONOZCO?', 
+        answer: 'Si no se encuentra el negocio o local que conoces es debido a que aún no se han dado de alta en Located App'
     },
     { 
         id: 3, 
-        question: '¿POR QUÉ NO PUEDO ACCEDER A TODAS LAS FUNCIONES DE LA APP?', 
-        answer: 'Si no puedes ingresar a todas las funciones de la app necesitas estar registrado o haber iniciado sesion '
+        question: '¿POR QUÉ ES MEJOR ESTAR REGISTRADO EN LOCATED APP?', 
+        answer: 'Estar registrado en Located App le brinda mayores funcionalidades, mostrarle negocios en base a sus gustos o tambíen los puede calificar, además de dar opiniones de los locales y/o negocios'
+    },
+    {   id: 4,
+        question:   '¿COBRAN POR REGISTAR UN LOCAL O UN NEGOCIO?',
+        answer:  'No, en Located App no se cobra por ningún registro, basta con llenar los formularios con la información correspondiente'
+    },
+    {   id: 5,
+        question:   '¿POR QUÉ DEBO DE DAR PERMISOS DE LOCALIZACION?',
+        answer:  'Es necesario para proporcionarle todos los datos de los negocios y locales que se encuentren cercas de usted'
     },
 ];
 
 export const QuestionsEn: AskAndAnswer[] = [
     {   
         id: 1, 
-        question: '¿POR QUÉ NO PUEDO ACCEDER A TODAS LAS FUNCIONES DE LA APP?', 
-        answer: 'Si no puedes ingresar a todas las funciones de la app necesitas estar registrado o haber iniciado sesion '
+        question: 'WHY CANNOT I ACCESS TO ALL THE FUNCTIONS OF THE APP?', 
+        answer: 'If you cannot access to all the functions of the app you need to be registered or have logged in'
     },
     {   
         id: 2,
-        question: '¿POR QUÉ NO PUEDO ACCEDER A TODAS LAS FUNCIONES DE LA APP?', 
-        answer: 'Si no puedes ingresar a todas las funciones de la app necesitas estar registrado o haber iniciado sesion '
+        question: 'WHY CANNOT YOU FIND A SPECIFIC BUSINESS OR LOCAL THAT YOU ALREADY KNOW?', 
+        answer: 'If a business or local that you know cannot be found, it is because they have not yet registered in the Located App'
     },
     { 
         id: 3, 
-        question: '¿POR QUÉ NO PUEDO ACCEDER A TODAS LAS FUNCIONES DE LA APP?', 
-        answer: 'Si no puedes ingresar a todas las funciones de la app necesitas estar registrado o haber iniciado sesion '
+        question: 'WHY IS IT BETTER TO BE REGISTERED IN LOCATED APP?', 
+        answer: 'Being registered in the Located App gives you more functionalities, showing you businesses based on your likes or you can also qualify them, in addition you can give your opinion of locals and/or businesses'
     },
-];
+    {   id: 4,
+        question:   'DO YOU CHARGE FOR REGISTERING A LOCAL OR A BUSINESS?',
+        answer:  'No, in Located App there is no charge for any registration, just fill out the forms with the corresponding information'
+    },
+    {   id: 5,
+        question:   'WHY SHOULD I GIVE LOCATION PERMISSIONS?',
+        answer:  'It is necessary to provide you all the data of the businesses and locals that are near you'
+    },
+];  
